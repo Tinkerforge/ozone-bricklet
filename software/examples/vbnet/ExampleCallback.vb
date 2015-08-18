89a341b344e5f8962e606053d3ffa3c4677165c3
Matthias Bolte <matthias@tinkerforge.com>
2015-08-18 17:05:43 +0200

    Cleanup VB.NET examples

--- software/examples/vbnet/ExampleCallback.vb
+++ software/examples/vbnet/ExampleCallback.vb
@@ -12,18 +12,18 @@ Module ExampleCallback
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
-        Dim oz As New BrickletOzone(UID, ipcon) ' Create device object
+        Dim o As New BrickletOzone(UID, ipcon) ' Create device object
 
         ipcon.Connect(HOST, PORT) ' Connect to brickd
         ' Don't use device before ipcon is connected
 
-        ' Set Period for ozone concentration callback to 1s (1000ms)
-        ' Note: The ozone concentration callback is only called every second if the
-        '       ozone concentration has changed since the last call!
-        oz.SetOzoneConcentrationCallbackPeriod(1000)
+        ' Set period for ozone concentration callback to 1s (1000ms)
+        ' Note: The ozone concentration callback is only called every second
+        '       if the ozone concentration has changed since the last call!
+        o.SetOzoneConcentrationCallbackPeriod(1000)
 
         ' Register ozone concentration callback to function OzoneConcentrationCB
-        AddHandler oz.OzoneConcentration, AddressOf OzoneConcentrationCB
+        AddHandler o.OzoneConcentration, AddressOf OzoneConcentrationCB
 
         System.Console.WriteLine("Press key to exit")
         System.Console.ReadLine()
--- software/examples/vbnet/ExampleThreshold.vb
+++ software/examples/vbnet/ExampleThreshold.vb
@@ -5,26 +5,26 @@ Module ExampleThreshold
     Const PORT As Integer = 4223
     Const UID As String = "XYZ" ' Change to your UID
 
-    ' Callback for ozone concentration greater than 20 ppb
-    Sub ReachedCB(ByVal sender As BrickletOzone, ByVal ozoneConcentration As Integer)
+    ' Callback function for ozone concentration greater than 20 ppb (parameter has unit ppb)
+    Sub OzoneConcentrationReachedCB(ByVal sender As BrickletOzone, ByVal ozoneConcentration As Integer)
         System.Console.WriteLine("Ozone Concentration: " + ozoneConcentration.ToString() + " ppb")
     End Sub
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
-        Dim oz As New BrickletOzone(UID, ipcon) ' Create device object
+        Dim o As New BrickletOzone(UID, ipcon) ' Create device object
 
         ipcon.Connect(HOST, PORT) ' Connect to brickd
         ' Don't use device before ipcon is connected
 
         ' Get threshold callbacks with a debounce time of 10 seconds (10000ms)
-        oz.SetDebouncePeriod(10000)
+        o.SetDebouncePeriod(10000)
 
-        ' Register threshold reached callback to function ReachedCB
-        AddHandler oz.OzoneConcentrationReached, AddressOf ReachedCB
+        ' Register threshold reached callback to function OzoneConcentrationReachedCB
+        AddHandler o.OzoneConcentrationReached, AddressOf OzoneConcentrationReachedCB
 
-        ' Configure threshold for "greater than 20 ppb"
-        oz.SetOzoneConcentrationCallbackThreshold(">"C, 20, 0)
+        ' Configure threshold for "greater than 20 ppb" (unit is ppb)
+        o.SetOzoneConcentrationCallbackThreshold(">"C, 20, 0)
 
         System.Console.WriteLine("Press key to exit")
         System.Console.ReadLine()
