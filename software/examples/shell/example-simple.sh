c42fae3c632687f78ce03d5c767014d73e4875a6
Matthias Bolte <matthias@tinkerforge.com>
2019-12-02 18:23:39 +0100

    Remove examples

--- software/examples/shell/example-simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Connects to localhost:4223 by default, use --host and --port to change this
-
-uid=XYZ # Change XYZ to the UID of your Ozone Bricklet
-
-# Get current ozone concentration
-tinkerforge call ozone-bricklet $uid get-ozone-concentration
